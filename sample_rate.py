deb2ee1534f2b1c70c7476034c6fee80c46a55ce
shambabanerjee3 <162182680+shambabanerjee3@users.noreply.github.com>
2024-03-04 23:49:21 +0530

    Create sample_rate.py
    this is code to calculate the sample rate from the audio file

---
+++ sample_rate.py
@@ -0,0 +1,12 @@
+def get_sample_rate_wav(audio_file_path):
+    with open(audio_file_path,'rb') as wav_file:
+        wav_file.seek(24)
+        sample_rate_bytes=wav_file.read(4)
+        sample_rate=int.from_bytes(sample_rate_bytes,byteorder='little')
+    return sample_rate
+
+audio_file_path='WhatsApp Audio 2024-03-04 at 11.27.40_69e1065f.mp3'
+
+sample_rate=get_sample_rate_wav(audio_file_path)
+
+print(sample_rate)
